4a4660f30844acb102305521a1e7df5508225ad6
Ramil Vildanov <ramvildan.work@gmail.com>
2022-12-02 17:09:36 +0400

    EmployeeListVerify.java UPD

--- src/main/java/ru/levelup/homework2/EmployeeListVerify.java
+++ src/main/java/ru/levelup/homework2/EmployeeListVerify.java
@@ -1,5 +1,20 @@
 package ru.levelup.homework2;
 
-public class EmployeeListVerify {
+import java.util.ArrayList;
+import java.util.List;
 
+public class EmployeeListVerify {
+    public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee("Ramen Supov", 1),
+                new Employee("Ivan Khan", 3),
+                new Employee("Gnom Gnomov", 6),
+                new Employee("Raz Razputin", 9),
+                new Employee("Gloria Jeans", 15)));
+
+        for (Employee employee : employees) {
+            Employee.printEmployee(employees, employee.getWorkAge());
+        }
+    }
 }
