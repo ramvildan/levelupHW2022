3849951a058bd9efa44edc752cdf8c648285b79c
Ramil Vildanov <ramvildan.work@gmail.com>
2022-12-06 22:40:55 +0400

    Correct printEmployee method

--- src/main/java/ru/levelup/homework2/Employee.java
+++ src/main/java/ru/levelup/homework2/Employee.java
@@ -2,35 +2,52 @@ package ru.levelup.homework2;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Data
 @RequiredArgsConstructor
 public class Employee {
-    private final String fullNAme;
+    private final String fullName;
     private final int workAge;
 
-    public static void printEmployee (Collection<Employee> employees) {
+    public static void printEmployee (Collection<Employee> employees, int necessaryWorkAge) {
 
-        for (Employee employee : employees) {
-            System.out.println(employee);
-        }
+        System.out.println("Suitable employees: ");
 
+//        for (Employee employee : employees) {
+//            if (employee.workAge >= necessaryWorkAge) {
+//                System.out.println(employee);
+//            }
+//        }
+//        без StreamAPI
+
+//        employees.removeIf(employee -> employee.workAge < necessaryWorkAge);
+//        System.out.println(employees);
+//        без StreamAPI при помощи Iterator
+
+        employees.stream()
+                .filter(employee -> employee.getWorkAge() >= necessaryWorkAge)
+                .forEach(System.out::println);
     }
 
-    public static void listIterator (List<Employee> employees) {
-
-        int counter = 0;
-        for (ListIterator<Employee> iterator = employees.listIterator(employees.size() - 1); iterator.hasPrevious(); ){
-            iterator.previous();
-            if (counter % 2 != 0) {
-                iterator.remove();
-            }
-            counter++;
-        }
-        System.out.println("Sorted list: " + employees);
-    }
+//    public static void listIterator (List<Employee> employees) {
+//
+//        for (ListIterator<Employee> iterator = employees.listIterator(employees.size()); iterator.hasPrevious(); ){
+//            iterator.next();
+//            int previous = iterator.previousIndex();
+//
+//            if (previous % 2 != 0) {
+//                iterator.remove();
+//            }
+//        }
+//        System.out.println("Sorted list: " + employees);
+//
+////        new LinkedList<>(employees)
+////                .descendingIterator()
+////                .forEachRemaining(System.out::println);
+//    }
 }
--- src/main/java/ru/levelup/homework2/EmployeeListVerify.java
+++ src/main/java/ru/levelup/homework2/EmployeeListVerify.java
@@ -1,19 +1,17 @@
 package ru.levelup.homework2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class EmployeeListVerify {
     public static void main(String[] args) {
 
-        List<Employee> employees = new ArrayList<>(List.of(
+        List<Employee> employees = new LinkedList<>((List.of(
                 new Employee("Ramen Supov", 1),
                 new Employee("Ivan Khan", 3),
                 new Employee("Gnom Gnomov", 6),
                 new Employee("Raz Razputin", 9),
-                new Employee("Gloria Jeans", 15)));
+                new Employee("Gloria Jeans", 2))));
 
-        Employee.printEmployee(employees);
-        Employee.listIterator(employees);
+        Employee.printEmployee(employees,3);
     }
 }
